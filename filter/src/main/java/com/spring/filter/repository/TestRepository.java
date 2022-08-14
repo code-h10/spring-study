2b15959bbf5b92787351eb4a0fc5122e244e0928
ilyounghwang <ilyoung6016@utransfer.com>
2022-08-14 14:06:06 +0900

    add service

--- filter/src/main/java/com/spring/filter/repository/TestRepository.java
+++ filter/src/main/java/com/spring/filter/repository/TestRepository.java
@@ -1,8 +1,12 @@
 package com.spring.filter.repository;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.mapstruct.Mapper;
 import org.springframework.stereotype.Repository;
 
+@Mapper
 @Repository
-public class TestRepository {
+public interface TestRepository {
 
 }
--- filter/src/main/java/com/spring/filter/service/TestService.java
+++ filter/src/main/java/com/spring/filter/service/TestService.java
@@ -1,7 +1,16 @@
 package com.spring.filter.service;
 
+import com.spring.filter.repository.TestRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
+
+    @Autowired private TestRepository testRepository;
+
+//    public Map findUserById(int id) {
+//        return testRepository.findUserById(id);
+//    }
+
 }
